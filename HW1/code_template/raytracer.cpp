155c3d6362ef9bd5c88a70dd6b5986102f85b2a0
BuseVarkan <busevarkan71@gmail.com>
2023-10-21 20:29:25 +0300

    hit added

--- HW1/code_template/raytracer.cpp
+++ HW1/code_template/raytracer.cpp
@@ -20,6 +20,7 @@ typedef struct Hit{
     Vec3f normal_vector;
     Vec3f intersection_point;
     int material_id;
+    int object_id;
 } hit;
 
 Vec3f normalization(const Vec3f vector){
@@ -95,6 +96,254 @@ vector<vector<Vec3f>> calculateMeshesNormalVectors(const Scene &scene){
     return mesh_normal_vectors;
 }
 
+float dotProduct(const Vec3f vector1, const Vec3f vector2){
+    float dot_product = vector1.x*vector2.x + vector1.y*vector2.y + vector1.z*vector2.z;
+    return dot_product;
+}
+
+vector<float> findRoots(float A, float B, float C){
+    vector<float> roots;
+    float discriminant = B*B - 4*A*C;
+    roots.push_back(discriminant);
+    if(discriminant >= 0){
+        float t1 = (-B + sqrtf(discriminant))/(2*A);
+        float t2 = (-B - sqrtf(discriminant))/(2*A);
+        roots.push_back(t1);
+        roots.push_back(t2);
+    }
+    return roots;
+}
+
+float calculateDeterminant(const Vec3f vector1, const Vec3f vector2, const Vec3f vector3){
+    float determinant = (vector1.x * (vector2.y*vector3.z - vector3.y*vector2.z)) + (vector1.y * (vector3.x*vector2.z - vector2.x*vector3.z)) + (vector1.z * (vector2.x*vector3.y - vector2.y*vector3.x));
+    return determinant;
+}
+
+Hit findClosestHit(vector<Hit> &all_hits){
+    Hit closest_hit;
+    int number_of_hits = all_hits.size();
+    if(number_of_hits > 0){
+        closest_hit.isHit = true;
+        closest_hit = all_hits[0];
+        for(int hit_index=1; hit_index<number_of_hits; hit_index++){
+            if(all_hits[hit_index].t < closest_hit.t){
+                closest_hit = all_hits[hit_index];
+            }
+        }
+    }
+    else{
+        closest_hit.isHit = false;
+    }
+    return closest_hit;
+}
+
+Hit operateHit(const Scene &scene, const Ray &ray, vector<Vec3f>triange_normal_vectors, vector<vector<Vec3f>>mesh_normal_vectors){
+    Triangle current_triangle;
+    Mesh current_mesh;
+    Sphere current_sphere;
+    Vec3f normal;
+    Vec3f intersection_point;
+
+    int number_of_spheres = scene.spheres.size();
+    int number_of_triangles = scene.triangles.size();
+    int number_of_meshes = scene.meshes.size();
+
+    vector<Hit> all_hits;
+
+    // Check hit for each triangle
+    for(int triangle_index = 0; triangle_index < number_of_triangles; triangle_index++){
+        current_triangle = scene.triangles[triangle_index];
+        Vec3f v0 = scene.vertex_data[current_triangle.indices.v0_id - 1];
+        Vec3f v1 = scene.vertex_data[current_triangle.indices.v1_id - 1];
+        Vec3f v2 = scene.vertex_data[current_triangle.indices.v2_id - 1];
+
+        Hit hit;
+        hit.isHit = true;
+
+        Vec3f o = ray.origin;
+        Vec3f d = ray.direction;
+
+        Vec3f a_minus_b = subtractVectors(v0, v1);
+        Vec3f a_minus_c = subtractVectors(v0, v2);
+        Vec3f a_minus_o = subtractVectors(v0, o);
+
+        float t;
+
+        float detA = calculateDeterminant(a_minus_b, a_minus_c, d);
+        
+
+        if(detA != 0.0){
+            t = calculateDeterminant(a_minus_b, a_minus_c, a_minus_o) / detA;
+            if(t>0){
+                float beta = calculateDeterminant(a_minus_o, a_minus_c, d) / detA;
+                if(beta >= 0 && beta <= 1){
+                    float gamma = calculateDeterminant(a_minus_b, a_minus_o, d) / detA;
+                    if(gamma >= 0 && gamma <= 1-beta){
+                        hit.t = t;
+                        hit.intersection_point.x = o.x + t*d.x;
+                        hit.intersection_point.y = o.y + t*d.y;
+                        hit.intersection_point.z = o.z + t*d.z;
+                        hit.normal_vector = triange_normal_vectors[triangle_index];
+                        hit.material_id = current_triangle.material_id;
+                        hit.object_id = triangle_index;
+                    }
+                    else{
+                        hit.isHit=false;
+                    }
+                }
+                else{
+                    hit.isHit=false;
+                }
+            }
+            else{
+                hit.isHit=false;
+            }
+        }
+
+        else{
+            hit.isHit=false;
+        }
+
+        if(hit.isHit && hit.t >= 0){
+            all_hits.push_back(hit);
+        }
+        
+    }
+
+
+    // Check hit for each sphere
+    for(int sphere_index = 0; sphere_index < number_of_spheres; sphere_index++){
+        current_sphere = scene.spheres[sphere_index];
+        Vec3f center = scene.vertex_data[current_sphere.center_vertex_id - 1];
+        float radius = current_sphere.radius;
+
+        Hit hit;
+
+        const float A = dotProduct(ray.direction, ray.direction);
+        Vec3f e_minus_c = subtractVectors(ray.origin, center);
+        const float B = 2 * dotProduct(ray.direction, e_minus_c);
+        const float C = dotProduct(e_minus_c, e_minus_c) - radius * radius;
+
+        vector<float> roots = findRoots(A, B, C);
+        if(roots.size() > 1){	
+            hit.isHit = true;
+
+            const float t1 = roots[1];
+            const float t2 = roots[2];
+
+            hit.material_id = current_sphere.material_id;
+            
+            //hit.objType = SPHERE;
+            hit.object_id = sphere_index;
+
+            const float t = (t1 < t2) ? t1 : t2;
+            hit.intersection_point.x = ray.origin.x + t*ray.direction.x;
+            hit.intersection_point.y = ray.origin.y + t*ray.direction.y;
+            hit.intersection_point.z = ray.origin.z + t*ray.direction.z;
+            hit.normal_vector = normalization(subtractVectors(hit.intersection_point, center)); //can be optimized with radius
+
+            hit.t = t;
+        }
+        else{
+            hit.isHit = false;
+        }
+
+        if(hit.isHit && hit.t >= 0){
+            all_hits.push_back(hit);
+        }
+    }
+
+
+
+    // Check hit for each mesh
+    for(int mesh_index = 0; mesh_index < number_of_meshes; mesh_index++){
+        current_mesh = scene.meshes[mesh_index];
+
+        Hit mesh_hit;
+	    mesh_hit.isHit = false;
+        vector<Hit> all_mesh_hits;
+
+        // FOR EACH FACE IN Current Mesh
+        for(int face_index = 0; face_index < current_mesh.faces.size(); face_index++){
+
+            Vec3f v0 = scene.vertex_data[current_mesh.faces[face_index].v0_id - 1];	
+            Vec3f v1 = scene.vertex_data[current_mesh.faces[face_index].v1_id - 1];
+            Vec3f v2 = scene.vertex_data[current_mesh.faces[face_index].v2_id - 1];
+
+            Hit face_hit;
+            face_hit.isHit = true;
+
+            Vec3f o = ray.origin;
+            Vec3f d = ray.direction;
+
+            Vec3f a_minus_b = subtractVectors(v0, v1);
+            Vec3f a_minus_c = subtractVectors(v0, v2);
+            Vec3f a_minus_o = subtractVectors(v0, o);
+
+            float t;
+
+            float detA = calculateDeterminant(a_minus_b, a_minus_c, d);
+            
+
+            if(detA != 0.0){
+                t = calculateDeterminant(a_minus_b, a_minus_c, a_minus_o) / detA;
+                if(t>0){
+                    float beta = calculateDeterminant(a_minus_o, a_minus_c, d) / detA;
+                    if(beta >= 0 && beta <= 1){
+                        float gamma = calculateDeterminant(a_minus_b, a_minus_o, d) / detA;
+                        if(gamma >= 0 && gamma <= 1-beta){
+                            face_hit.t = t;
+                            face_hit.intersection_point.x = o.x + t*d.x;
+                            face_hit.intersection_point.y = o.y + t*d.y;
+                            face_hit.intersection_point.z = o.z + t*d.z;
+                            face_hit.normal_vector = mesh_normal_vectors[mesh_index][face_index];
+                            face_hit.material_id = current_triangle.material_id;
+                            face_hit.object_id = mesh_index;
+                        }
+                        else{
+                            face_hit.isHit=false;
+                        }
+                    }
+                    else{
+                        face_hit.isHit=false;
+                    }
+                }
+                else{
+                    face_hit.isHit=false;
+                }
+            }
+
+            else{
+                face_hit.isHit=false;
+            }
+
+            if(face_hit.isHit && face_hit.t >= 0)
+            {
+                face_hit.material_id = current_mesh.material_id;
+                //face_hit.objType = MESH;
+                face_hit.object_id = mesh_index;
+                face_hit.intersection_point.x = o.x + t*d.x;
+                face_hit.intersection_point.y = o.y + t*d.y;
+                face_hit.intersection_point.z = o.z + t*d.z;
+                face_hit.normal_vector = mesh_normal_vectors[mesh_index][face_index];
+
+                all_mesh_hits.push_back(face_hit);
+            }
+        }
+
+        mesh_hit = findClosestHit(all_mesh_hits);
+
+        if(mesh_hit.isHit && mesh_hit.t >= 0){
+            all_hits.push_back(mesh_hit);
+        }
+    }
+
+
+    Hit hitResult = findClosestHit(all_hits);
+
+    return hitResult;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -144,12 +393,12 @@ int main(int argc, char* argv[])
         for(int i=0; i<camera.image_height; i++){
             for(int j=0; j<camera.image_width; j++){
                 Ray ray = createRay(i, j, camera, image_top_left, px, py, normalized_camera_u, normalized_camera_v);
-                
+                Hit hit = operateHit(scene, ray, triangle_normal_vectors, mesh_normal_vectors);
             }
         }
 
 
-
+        write_ppm(camera.image_name.c_str(),image, camera.image_width, camera.image_height);
     }
     
 
